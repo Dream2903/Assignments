33d62578c24453ec86ff318bf3a2e1e37f40fe09
Dream2903 <70510231+Dream2903@users.noreply.github.com>
2020-09-28 21:09:11 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,41 @@
+public class Car {
+
+    //Attributes
+    String brand;
+    String color = "red";
+    String type = "diesel";
+
+    int topspeed;
+    int speed = 0;
+
+    //Constructor
+    public Car(String brandname, int ts) {
+        brand = brandname;
+        topspeed = ts;
+    }
+
+    public Car(int ts) {
+        topspeed = ts;
+        brand = "zenvo";
+    }
+
+    public void setSpeed(int i) {
+
+        if (i > topspeed) {
+            speed = topspeed;
+        } else {
+            speed = i;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Car c = new Car (180);
+        System.out.println(c.speed);
+        c.setSpeed(5000);
+        System.out.println(c.speed);
+        c.setSpeed(100);
+        System.out.println(c.speed);
+    }
+
+}
